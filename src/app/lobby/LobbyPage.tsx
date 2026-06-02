9a98e4f2b583b929dbb20816fcaee1538f1e92c1
psyao13 <psyao13@exmail.nottingham.ac.uk>
2026-06-02 18:41:10 +0100

    Fix casing issues

---
+++ src/app/lobby/LobbyPage.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import LobbyPage from "../../components/LobbyPage";
+
+export default function lobbyPage() {
+  return <LobbyPage />;
+}
--- src/app/lobby/lobbyPage.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import LobbyPage from "../../components/Lobbypage";
-
-export default function Page() {
-  return <LobbyPage />;
-}
